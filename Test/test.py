1960f5985d3427567f9ffb1aedb70e0e6e61592f
PyEddy <eduardomoreira.london@gmail.com>
2023-02-17 18:04:15 +0000

    Update test.py

--- Test/test.py
+++ Test/test.py
@@ -1,6 +1,12 @@
 from nintendeals import noa, noe, noj
 from commands import printallregions, listallregions
+import sys
+
+
+sys.path.insert(0, '/commands')
 
 def runTest():
     printallregions.printAllRegions()
     listallregions.listAllRegions()
+
+runTest()
