21ac300e9c3229d1582301122df5abac3426d91c
PyEddy <eduardomoreira.london@gmail.com>
2023-08-28 21:07:15 +0100

    more code cleanup

--- commands/region.py
+++ commands/region.py
@@ -6,13 +6,19 @@ def region(command):
         print("Invalid use of command region")
     if command == "region -noa":
         for game in noa.list_switch_games():
-            print(game.title, "/", game.nsuid)
+            Region = "Nintendo of America: "
+            gameDetails = ("{} {} / {}".format(Region, game.title, game.nsuid))
+            print(gameDetails)
     if command == "region -noe":
         for game in noe.list_switch_games():
-            print(game.title, "/", game.nsuid)
+            Region = "Nintendo of Europe: "
+            gameDetails = ("{} {} / {}".format(Region, game.title, game.nsuid))
+            print(gameDetails)
     if command == "region -noj":
         for game in noj.list_switch_games():
-            print(game.title, "/", game.nsuid)
+            Region = "Nintendo of Japan: "
+            gameDetails = ("{} {} / {}".format(Region, game.title, game.nsuid))
+            print(gameDetails)
     if command == "region -p -noa":
         for game in noa.list_switch_games():
             with open('NintendoOfAmerica.txt', 'a', encoding="utf-8") as f:
