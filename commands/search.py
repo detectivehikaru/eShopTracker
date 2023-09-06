1f395399dc5cf850fe8bd1792b547528ee48f9e6
PyEddy <eduardomoreira.london@gmail.com>
2023-09-06 22:45:55 +0100

    Add the JP region
    fixes #1

--- commands/search.py
+++ commands/search.py
@@ -3,11 +3,13 @@ from nintendeals import noa, noe, noj
 
 def searchedItem(Item):
     for game in noa.search_switch_games(query="{}".format(Item)):
-        print(game.title, "/", game.nsuid)
+        if Item in game.title:
+            print(game.title, "/", game.nsuid, "NA")
     for game in noe.search_switch_games(query="{}".format(Item)):
-        print(game.title, "/", game.nsuid)
-    #for game in noj.search_switch_games(query="{}".format(Item)):
-    #    print(game.title, "/", game.nsuid)
+        if Item in game.title:
+            print(game.title, "/", game.nsuid, "EU")
+    for game in noj.search_switch_games(query="{}".format(Item)):
+        print(game.title, "/", game.nsuid, "JP")
 
 
 def search():
